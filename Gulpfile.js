e2f3c291de42a37c33eebc9e1c94daeddd01327b
Zack Ginies <zack@zackginies.com>
2016-04-03 21:12:16 -0400

    Add new layout // Add normalize // Add haml to gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var haml = require('gulp-haml');
 var sass = require('gulp-sass');
+
+gulp.task('haml', function () {
+  gulp.src('./*.haml')
+    .pipe(haml())
+    .pipe(gulp.dest('./'));
+});
 
 gulp.task('styles', function() {
   gulp.src('assets/sass/**/*.scss')
@@ -9,5 +16,6 @@ gulp.task('styles', function() {
 
 //Watch task
 gulp.task('default', function() {
-  gulp.watch('assets/sass/**/*.scss',['styles']);
+  gulp.watch('./assets/sass/*.scss',['styles']);
+  gulp.watch('./*.haml',['haml']);
 });
